9855930f12b9bf18d937c64c04cf4075f3364a18
mjhuang23 <35316705+mjhuang23@users.noreply.github.com>
2019-11-26 17:38:57 -0500

    Add files via upload

--- Syslab.java
+++ Syslab.java
@@ -3,28 +3,28 @@ import java.util.*;
 public class Syslab{
    public static void main(String[] args) throws IOException
    {
-      int[] test = new int[]{-2,-1,0,0,1,2};
-      ArrayList<Vector> basis = generate(test);
+      int[] test = new int[]{-1,0,1};// Test is the basis set that we will be cubing.
+      ArrayList<Vector> basis = generate(test);// This takes test and generates our set of vectors.
       Collections.sort(basis);
       System.out.println(basis);
-      int[][] adj = new int[basis.size()][basis.size()];
+      int[][] adj = new int[basis.size()][basis.size()];// This creates our adjacency matrix
       int count = 0;
       for(int i = 0; i< basis.size();i++){
          for(int j = i; j< basis.size();j++){
             if(basis.get(i).ortho(basis.get(j))){
                adj[i][j] = 1;
                adj[j][i] = 1;
-               count++;
+               count++;// This counts the number of edges, this can act as a heuristic as denser graphs will have smaller MIS
             }
          }
       }
       System.out.println(count);
-      int size = (basis.size() * (basis.size()-1))/2;
+      int size = (basis.size() * (basis.size()-1))/2;// Maximum possible number of edges
       System.out.println(size);
       File out = new File("output.txt");
       FileWriter fWriter = new FileWriter (out);
       PrintWriter pWriter = new PrintWriter (fWriter);
-      pWriter.print("g = AdjacencyGraph[{{");
+      pWriter.print("g = AdjacencyGraph[{{");//Formatting for Mathematica
       for(int i = 0; i<adj.length; i++){
          if(i != 0) {
             pWriter.println(",");
@@ -54,7 +54,7 @@ public class Syslab{
          for(int j: nums){
             for(int k: nums){
                if(i != 0 || j != 0 || k!= 0){
-                  al.add(new Vector(new int[]{i,j,k}));
+                  al.add(new Vector(new int[]{i,j,k}));//Generates all possible ones except for (0,0,0)
                }
             }
          }
@@ -63,7 +63,7 @@ public class Syslab{
    }
 }
 class Vector implements Comparable<Vector>{
-   public final static int size = 3; 
+   public final static int size = 3; // Our class of vectors, working in 3 dimensions.
    int[] pos;
    Vector(int[] a){
       pos = a;
@@ -71,7 +71,7 @@ class Vector implements Comparable<Vector>{
          System.out.println("Error");
       }
    }
-   public int dotP (Vector other){
+   public int dotP (Vector other){// Orthogonality implies a dot product of 0
       int ret = 0;
       for(int i = 0; i< size; i++){
          ret+=pos[i]*other.pos[i];
@@ -82,7 +82,7 @@ class Vector implements Comparable<Vector>{
       return (this.dotP(other) == 0);
    }
    public String toString(){
-      return Arrays.toString(pos);
+      return Arrays.toString(pos);// Formatting
    }
    public int compareTo(Vector other){
       if(pos[0]<other.pos[0])
